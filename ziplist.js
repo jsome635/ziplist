c5f86010a6531a4ae57289346cb5ec07045caac6
Jacob Somera <jsome635@hawaii.edu>
2018-02-05 21:04:24 -1000

    Added file for attempt 2.

--- ziplist.js
+++ ziplist.js
@@ -1,20 +1,20 @@
 const testOne = ['a', 'b', 'c'];
-const testTwo = [1, 2, 3];
+const testTwo = [1, 2, 3,];
 
 function zipList(arrayOne, arrayTwo) {
-  const zipArray = [];
+  const zippedList = [];
   for (let count = 0; count < arrayOne.length; count++) {
-    zipArray[2 * count] = arrayOne[count];
-    zipArray[(2 * count) + 1] = arrayTwo[count];
+    zippedList[2 * count] = arrayOne[count];
+    zippedList[(2 * count) + 1] = arrayTwo[count];
   }
-  return zipArray;
+  return zippedList;
 }
 
 console.log(zipList(testOne, testTwo));
 
 function zipListTheSimpleWay(arrayOne, arrayTwo) {
-  const nestedArrays = _.zip(arrayOne, arrayTwo);
-  return _.union(_.flatten(nestedArrays));
+  const nestedList = _.zip(arrayOne, arrayTwo);
+  return _.union(_.flatten(nestedList));
 }
 
 console.log(zipListTheSimpleWay(testOne, testTwo));
